858547ab71b328db1ad0f38f5f1a87959f949e48
anandku06 <161126335+anandku06@users.noreply.github.com>
2025-04-05 19:27:28 +0530

    Initialize project structure with main application, routes, models, and configuration files

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"context"
+	"log"
+	"time"
+	"os"
+
+	"github.com/anandku06/GoDataHub/config"
+	"github.com/anandku06/GoDataHub/routes"
+)
+
+func main() {
+		
+}
