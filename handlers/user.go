5687ed7523e6a5735859c3e050b95948cef78307
anandku06 <161126335+anandku06@users.noreply.github.com>
2025-04-22 01:19:41 +0530

    Implement CreateUser handler for user registration with MongoDB integration

---
+++ handlers/user.go
@@ -0,0 +1,39 @@
+package handlers
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/anandku06/GoDataHub/config"
+	"github.com/anandku06/GoDataHub/models"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"golang.org/x/tools/go/analysis/passes/defers"
+)
+
+func CreateUser(w http.ResponseWriter, r *http.Request) {
+	client, err := config.ConnectToMongoDB()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer client.Disconnect(context.Background())
+
+	var user models.User
+	if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	collection := client.Database("mydatabase").Collection("users")
+	result, err := collection.InsertOne(context.Background(), user)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	json.NewEncoder(w).Encode(result)
+}
